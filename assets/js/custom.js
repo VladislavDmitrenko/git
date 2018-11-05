2c21ea44c340ba7c9dfb52fa4d973346ee7d3ed1
Vlad Dmitrenko <yellow31031995@gmail.com>
2018-11-05 22:48:00 +0200

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+(function ($) {  
+
+
+  $(document).ready(function() {  
+      $("html").niceScroll();
+  });
+
+
+  $(".header-menu__btn").click(function() {
+    $(this).toggleClass("active");
+    $(".header-menu__list").toggleClass("active");
+  });
+
+
+})(jQuery);
